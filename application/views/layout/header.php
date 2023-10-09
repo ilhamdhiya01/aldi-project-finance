1801c5e52d2bb278dae3280fe703be1fa945adcf
Ilham Dhiya Ulhaq <ulhaqilhamdhiya@gmail.com>
2023-10-09 23:23:11 +0700

    first commit dev

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -14,7 +14,7 @@
   <!-- Theme style -->
   <link rel="stylesheet" href="dist/css/adminlte.min.css">
 </head>
-<body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+<body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed">
   <div class="wrapper">
     <!-- Preloader -->
     <div class="preloader flex-column justify-content-center align-items-center">
--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -1,9 +1,9 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
-  <a href="index3.html" class="brand-link">
-    <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-    <span class="brand-text font-weight-light">AdminLTE 3</span>
+  <a href="index3.html" class="brand-link d-flex justify-content-center">
+    <!-- <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> -->
+    <span class="brand-text font-weight-bold">Finance Management</span>
   </a>
 
   <!-- Sidebar -->
@@ -24,10 +24,20 @@
         <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
         <!-- <li class="nav-item menu-open"> -->
         <li class="nav-item">
-          <a href="#" class="nav-link active">
+          <a href="<?= base_url()?>/dashboard" class="nav-link">
             <i class="nav-icon fas fa-tachometer-alt"></i>
             <p>
               Dashboard
+              <!-- <span class="right badge badge-danger">New</span> -->
+            </p>
+          </a>
+        </li>
+        <li class="nav-header">FINANCE</li>
+        <li class="nav-item">
+          <a href="#" class="nav-link active">
+            <i class="nav-icon fas fa-tachometer-alt"></i>
+            <p>
+              Transaction
               <i class="right fas fa-angle-left"></i>
             </p>
           </a>
@@ -52,16 +62,6 @@
             </li>
           </ul>
         </li>
-        <li class="nav-item">
-          <a href="pages/widgets.html" class="nav-link">
-            <i class="nav-icon fas fa-th"></i>
-            <p>
-              Widgets
-              <span class="right badge badge-danger">New</span>
-            </p>
-          </a>
-        </li>
-        <li class="nav-header">EXAMPLES</li>
       </ul>
     </nav>
   </div>
