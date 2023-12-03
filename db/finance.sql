e8707cbfc535d7e1ab3698417eca320374d2ea4c
Ilham Dhiya Ulhaq <ulhaqilhamdhiya@gmail.com>
2023-12-03 12:55:20 +0700

    all done

--- db/finance.sql
+++ db/finance.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 10, 2023 at 06:48 PM
+-- Generation Time: Dec 03, 2023 at 06:54 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.1
 
@@ -21,6 +21,73 @@ SET time_zone = "+00:00";
 --
 -- Database: `finance`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `finance`
+--
+
+CREATE TABLE `finance` (
+  `id` int(11) NOT NULL,
+  `information` varchar(50) DEFAULT NULL,
+  `recordDate` varchar(50) DEFAULT NULL,
+  `cashIn` int(11) DEFAULT NULL,
+  `cashOut` int(11) DEFAULT NULL,
+  `createdDate` timestamp NULL DEFAULT NULL,
+  `updatedDate` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `finance`
+--
+
+INSERT INTO `finance` (`id`, `information`, `recordDate`, `cashIn`, `cashOut`, `createdDate`, `updatedDate`) VALUES
+(2, 'Piutang dari PT Global Sukses Solusi Tbk', '2023-11-19', 35000000, NULL, '2023-11-19 04:42:39', '2023-11-19 05:18:23'),
+(4, 'Pembayarn Utang PT SAMI', '2023-11-19', NULL, 12500000, '2023-11-19 06:11:37', NULL),
+(5, 'Cicilan piutang dari PT Global Sukses Solusi', '2023-11-27', 13000000, NULL, '2023-11-27 13:48:45', NULL),
+(6, 'Cicilan piutang dari PT Global Sukses Solusi', '2023-10-27', 25000000, NULL, '2023-11-27 13:49:51', NULL),
+(7, 'Cicilan piutang dari PT Global Sukses Solusi', '2023-12-27', 30000000, NULL, '2023-11-27 13:50:55', NULL),
+(8, 'Cicilan piutang dari PT Global Sukses Solusi', '2024-01-27', 147000000, NULL, '2023-11-27 13:51:37', NULL),
+(9, 'Cicilan piutang dari PT Global Sukses Solusi', '2024-02-27', 15000000, NULL, '2023-11-27 13:54:22', NULL),
+(10, 'Pembayarn Utang PT SAMI', '2023-11-27', NULL, 23000000, '2023-11-27 14:10:00', NULL),
+(11, 'Keuntungan pengantaran bualn Oktober', '2023-12-03', 38000000, NULL, '2023-12-03 05:52:32', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `piutang`
+--
+
+CREATE TABLE `piutang` (
+  `id` int(11) NOT NULL,
+  `referenceNumber` varchar(50) DEFAULT NULL,
+  `debtorName` varchar(50) DEFAULT NULL,
+  `recordDate` varchar(50) DEFAULT NULL,
+  `dueDate` varchar(50) DEFAULT NULL,
+  `totalReceivable` int(11) DEFAULT NULL,
+  `lastPaymentDate` varchar(50) DEFAULT NULL,
+  `currentReceivable` int(11) DEFAULT NULL,
+  `initialReceivable` int(11) DEFAULT NULL,
+  `createdDate` timestamp NULL DEFAULT NULL,
+  `updatedDate` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `piutang`
+--
+
+INSERT INTO `piutang` (`id`, `referenceNumber`, `debtorName`, `recordDate`, `dueDate`, `totalReceivable`, `lastPaymentDate`, `currentReceivable`, `initialReceivable`, `createdDate`, `updatedDate`) VALUES
+(22, '#PIU231111357', 'PT SIM', '2023-11-11', '2023-11-11', NULL, '2024-01-12', 0, 300000000, '2023-11-11 16:38:23', '2024-01-11 17:00:00'),
+(23, '#PIU231111357', 'PT SIM', '2023-12-11', NULL, 15000000, NULL, NULL, NULL, '2023-11-11 16:41:51', NULL),
+(24, '#PIU23111115', 'PT Global Sukses Solusi', '2023-11-11', '2024-11-11', NULL, '2024-02-27', 0, 250000000, '2023-11-11 16:59:47', '2024-02-26 17:00:00'),
+(26, '#PIU231111357', 'PT SIM', '2024-01-12', NULL, 285000000, NULL, NULL, NULL, '2023-11-11 17:04:10', NULL),
+(31, '#PIU23111115', 'PT Global Sukses Solusi', '2023-11-19', NULL, 20000000, NULL, NULL, NULL, '2023-11-19 04:26:56', NULL),
+(32, '#PIU23111115', 'PT Global Sukses Solusi', '2023-11-27', NULL, 13000000, NULL, NULL, NULL, '2023-11-27 13:48:45', NULL),
+(33, '#PIU23111115', 'PT Global Sukses Solusi', '2023-10-27', NULL, 25000000, NULL, NULL, NULL, '2023-11-27 13:49:51', NULL),
+(34, '#PIU23111115', 'PT Global Sukses Solusi', '2023-12-27', NULL, 30000000, NULL, NULL, NULL, '2023-11-27 13:50:55', NULL),
+(35, '#PIU23111115', 'PT Global Sukses Solusi', '2024-01-27', NULL, 147000000, NULL, NULL, NULL, '2023-11-27 13:51:37', NULL),
+(36, '#PIU23111115', 'PT Global Sukses Solusi', '2024-02-27', NULL, 15000000, NULL, NULL, NULL, '2023-11-27 13:54:22', NULL);
 
 -- --------------------------------------------------------
 
@@ -57,26 +124,14 @@ INSERT INTO `tbloption` (`id`, `optDesc`, `optCode`, `optStatus`) VALUES
 (15, 'UserStatus', '01', 'Active'),
 (16, 'UserStatus', '02', 'Unactive'),
 (17, 'RoleUser', '01', 'Super Admin'),
-(18, 'RoleUser', '02', 'Admin');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `transaction`
---
-
-CREATE TABLE `transaction` (
-  `id` int(11) NOT NULL,
-  `total` int(11) DEFAULT NULL,
-  `vendorId` int(11) DEFAULT NULL,
-  `description` varchar(50) DEFAULT NULL,
-  `typeId` char(50) DEFAULT NULL,
-  `paymentMethodId` char(50) DEFAULT NULL,
-  `paymentStatusId` char(50) DEFAULT NULL,
-  `verificationStatusId` char(50) DEFAULT NULL,
-  `createDt` timestamp NULL DEFAULT NULL,
-  `updateDt` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+(18, 'RoleUser', '02', 'Admin'),
+(19, 'VehicleStatus', '01', 'Siap digunakan'),
+(20, 'VehicleStatus', '02', 'Dalam perbaikan'),
+(21, 'VehicleStatus', '03', 'Sedang digunakan'),
+(22, 'PiutangStatus', '01', 'Jatuh tempo'),
+(23, 'PiutangStatus', '02', 'Sudah terbayar'),
+(24, 'PiutangStatus', '03', 'Terlambat'),
+(25, 'PiutangStatus', '04', 'Dalam Proses');
 
 -- --------------------------------------------------------
 
@@ -100,15 +155,67 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `username`, `password`, `status`, `roleId`, `createDt`, `updateDt`) VALUES
-(1, 'Ilham Dhiya Ulhaq', 'ilhamdhiya01', 'ilhamdhiya01', '01', '01', NULL, NULL);
+(3, 'Annisa  Wahyu Hidayah', 'annisawahyu01', '$2y$10$oxHo9IdtKrqkp6CAGP/GnezevwfQZNEOHIABvHjLDUbsXD9xneA/6', '01', '02', '2023-10-12 14:22:43', '2023-12-03 05:48:44'),
+(4, 'Ilham Dhiya Ulhaq', 'ilhamdhiya01', '$2y$10$Dod49gOwZ.byMj0NwCbN0eJs3eSy1jPSLD/pSRZtWruWRdLHQXShi', '01', '01', '2023-10-14 15:44:59', '2023-10-14 16:24:07'),
+(5, 'Frido', 'frido01', '$2y$10$v1eqKLL8K6QBSohRHWFfleQmVmDkyC1UwXDsk8kAAzSiu2zU2BAyS', '01', '02', '2023-10-14 16:41:56', '2023-12-03 05:48:54');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `vendor`
+-- Table structure for table `utang`
 --
 
-CREATE TABLE `vendor` (
+CREATE TABLE `utang` (
+  `id` int(11) NOT NULL,
+  `referenceNumber` varchar(50) DEFAULT NULL,
+  `creditorName` varchar(50) DEFAULT NULL,
+  `recordDate` varchar(50) DEFAULT NULL,
+  `dueDate` varchar(50) DEFAULT NULL,
+  `totalDebt` int(11) DEFAULT NULL,
+  `lastPaymentDate` varchar(50) DEFAULT NULL,
+  `currentDebt` int(11) DEFAULT NULL,
+  `initialDebt` int(11) DEFAULT NULL,
+  `createdDate` timestamp NULL DEFAULT NULL,
+  `updatedDate` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `utang`
+--
+
+INSERT INTO `utang` (`id`, `referenceNumber`, `creditorName`, `recordDate`, `dueDate`, `totalDebt`, `lastPaymentDate`, `currentDebt`, `initialDebt`, `createdDate`, `updatedDate`) VALUES
+(1, '#UTG231112351', 'BRI', '2023-11-12', '2025-11-12', NULL, '2023-12-12', 500000000, 500000000, '2023-11-12 15:31:39', '2023-11-12 16:10:13');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vehicles`
+--
+
+CREATE TABLE `vehicles` (
+  `id` int(11) NOT NULL,
+  `type` varchar(50) DEFAULT '0',
+  `numberPlate` varchar(50) DEFAULT '0',
+  `lastService` varchar(50) DEFAULT NULL,
+  `serviceAgain` varchar(50) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `vehicles`
+--
+
+INSERT INTO `vehicles` (`id`, `type`, `numberPlate`, `lastService`, `serviceAgain`, `status`) VALUES
+(1, 'Sigra', 'H 6011 JW', '2023-10-20', '2023-12-20', '01'),
+(3, 'Test', 'T 569 JY', '2023-12-03', '2024-01-01', '01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vendors`
+--
+
+CREATE TABLE `vendors` (
   `id` int(11) NOT NULL,
   `name` varchar(50) DEFAULT NULL,
   `phone` varchar(50) DEFAULT NULL,
@@ -117,19 +224,33 @@ CREATE TABLE `vendor` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `vendors`
+--
+
+INSERT INTO `vendors` (`id`, `name`, `phone`, `address`, `vendorStatus`) VALUES
+(1, 'PT. Poliplas Tbk', '08123456789', 'Jl Kawasan Industri Candi No 502 C', '01'),
+(5, 'PT. Global Sukses Solusi Tbk', '089765432173', 'Jl. Cokrodiningrat No 15 B', '02');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `finance`
+--
+ALTER TABLE `finance`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `piutang`
+--
+ALTER TABLE `piutang`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tbloption`
 --
 ALTER TABLE `tbloption`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `transaction`
---
-ALTER TABLE `transaction`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -140,9 +261,21 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `email` (`username`) USING BTREE;
 
 --
--- Indexes for table `vendor`
+-- Indexes for table `utang`
 --
-ALTER TABLE `vendor`
+ALTER TABLE `utang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `vehicles`
+--
+ALTER TABLE `vehicles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `vendors`
+--
+ALTER TABLE `vendors`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -150,28 +283,46 @@ ALTER TABLE `vendor`
 --
 
 --
+-- AUTO_INCREMENT for table `finance`
+--
+ALTER TABLE `finance`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `piutang`
+--
+ALTER TABLE `piutang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+
+--
 -- AUTO_INCREMENT for table `tbloption`
 --
 ALTER TABLE `tbloption`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
-
---
--- AUTO_INCREMENT for table `transaction`
---
-ALTER TABLE `transaction`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `vendor`
+-- AUTO_INCREMENT for table `utang`
 --
-ALTER TABLE `vendor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `utang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `vehicles`
+--
+ALTER TABLE `vehicles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `vendors`
+--
+ALTER TABLE `vendors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
